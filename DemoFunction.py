a29d45e981dbd087b3e32ce49f62e81a30964a31
hansol2755 <hansol2755@naver.com>
2021-04-05 17:13:03 +0900

    전부 추가

---
+++ DemoFunction.py
@@ -0,0 +1,16 @@
+# DemoFunction.py
+# 함수를 정의
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+
+#호출
+print( add(3,4))
+print( sub(5,2))
+print( mul(5,2))
+print( divide(5,2))
